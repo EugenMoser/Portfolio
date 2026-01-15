72cdfffdc27eefa0a32df49f44e7bc0785457013
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2026-01-15 19:52:40 +0100

    update to next 16

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+import tailwindcss from "@tailwindcss/postcss";
 
-export default config;
+export default {
+  plugins: [
+    tailwindcss,
+    // ...weitere Plugins
+  ],
+};
